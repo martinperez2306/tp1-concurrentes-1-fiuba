8560427333ddc9425e80f48672675b6c0da41ef3
Martin Nicolas Perez <manperez@fi.uba.ar>
2021-09-28 19:31:00 -0300

    Agrego funcionalidad en main para leer un archivo de texto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    // --snip--
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
