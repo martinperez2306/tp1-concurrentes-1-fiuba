4d383b2b1ebbf69795d9ff6eac7eb7a1acf9dba3
Martin Nicolas Perez <manperez@fi.uba.ar>
2021-09-28 20:18:13 -0300

    Lectura por linea, lanzo un  thread para imprimir cada reserva por consola

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::thread;
 
 fn main() {
+
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    // --snip--
-    println!("In file {}", filename);
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(reserva) = line {
+                children.push(thread::spawn(move || {
+                    println!("A new threas is reading the line {}", reserva);
+                }));
+            }
+        }
+    }
+}
 
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
