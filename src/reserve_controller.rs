399d5e4b0ab5a3b492a13973632cc28214f753a9
manudelca <manueldelcarril@gmail.com>
2021-10-04 09:13:28 -0300

    Fixed clippy

--- src/reserve_controller.rs
+++ src/reserve_controller.rs
@@ -14,8 +14,8 @@ pub struct Reserve {
 
 const DELAY_BETWEEN_RETRIES: u64 = 5;
 
-pub fn reserve_airline(origin: &String, destination: &String){
-    let approved: bool = webservice_aerolineas::reservar(origin.clone(), destination.clone());
+pub fn reserve_airline(origin: &str, destination: &str){
+    let approved: bool = webservice_aerolineas::reservar(origin.to_string(), destination.to_string());
     if !approved {
         println!("La aerolinea no aprobó la reserva. Reintentando en {} segundos", DELAY_BETWEEN_RETRIES);
         thread::sleep(Duration::from_millis(DELAY_BETWEEN_RETRIES*1000));
@@ -26,8 +26,8 @@ pub fn reserve_airline(origin: &String, destination: &String){
     println!("La aerolinea aprobó la reserva con origen: {} y destino: {}", origin, destination);
 }
 
-pub fn reserve_hotel(hotel: &String){
-    webservice_hoteles::reservar(hotel.clone());
+pub fn reserve_hotel(hotel: &str){
+    webservice_hoteles::reservar(hotel.to_string());
     println!("El servicio de hoteles aprobó la reserva en: {}", hotel);
 }
 
@@ -37,20 +37,18 @@ pub fn process_reserve(reserve: Reserve){
     reserve_hotel(&reserve.hotel);
 }
 
-pub fn parse_reserves(filename: &String){
+pub fn parse_reserves(filename: &str){
     // Make a vector to hold the children which are spawned.
     let mut children = vec![];
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
-        for line in lines {
-            if let Ok(reserve_line) = line {
-                let reserve_split: Vec<&str> = reserve_line.split(" ").collect();
-                let reserve = Reserve{origin: reserve_split[0].to_string(),
-                    destination: reserve_split[1].to_string(),
-                    airline: reserve_split[2].to_string(),
-                    hotel: reserve_split[3].to_string()};
-                children.push(thread::spawn(move || process_reserve(reserve)));
-            }
+        for reserve_line in lines.into_iter().flatten() {
+            let reserve_split: Vec<&str> = reserve_line.split(' ').collect();
+            let reserve = Reserve{origin: reserve_split[0].to_string(),
+                destination: reserve_split[1].to_string(),
+                airline: reserve_split[2].to_string(),
+                hotel: reserve_split[3].to_string()};
+            children.push(thread::spawn(move || process_reserve(reserve)));
         }
     }
 
--- src/webservice_aerolineas.rs
+++ src/webservice_aerolineas.rs
@@ -6,5 +6,5 @@ pub fn reservar(_origen: String, _destino: String) -> bool {
     let seconds_to_sleep = rng.gen_range(0..10);
     thread::sleep(Duration::from_millis(seconds_to_sleep*1000));
     let random_response = rng.gen_range(0..10);
-    return random_response <= 5
+    random_response <= 5
 }
