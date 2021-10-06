3181596a50c523603550fdca965264507474285f
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-10-06 20:23:39 -0300

    Agrego logger para imprimir por consola y loguear en un archivo de log

---
+++ src/reserve_controller/model/logger.rs
@@ -0,0 +1,10 @@
+use std::io::Write;
+use std::fs::OpenOptions;
+
+pub fn log(message: String){
+    let mut file = OpenOptions::new().write(true).create(true).append(true).open("resoruces/reserves.log").unwrap();
+    println!("{}", message);
+    if let Err(e) = writeln!(file, "{}", message){
+        println!("Couldn't write to file: {}", e);
+    };
+}
