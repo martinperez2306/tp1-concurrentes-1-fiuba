317fa3d7fb8da44261a51bbace508d86e1dd9406
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-10-23 19:06:03 -0300

    Agrego exports de model

--- tp1-parte-b/src/main.rs
+++ tp1-parte-b/src/main.rs
@@ -1,17 +1,15 @@
 mod model;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix::prelude::*;
+use model::receiver_actor::ReserveString;
+use crate::model::ping_actor::PingActor;
+use crate::model::ping_actor::Ping;
 use crate::model::receiver_actor::ReceiverActor;
-use crate::model::receiver_actor::Ping;
+
 
 #[get("/ping")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Pong!")
-}
-
-#[post("/reserves")]
-async fn echo(req_body: String) -> impl Responder {
-    let addr = ReceiverActor.start();
+async fn ping() -> impl Responder {
+    let addr = PingActor.start();
     // Send Ping message.
     // send() message returns Future object, that resolves to message result
     let result = addr.send(Ping).await;
@@ -20,20 +18,32 @@ async fn echo(req_body: String) -> impl Responder {
         Ok(res) => println!("Got result: {}", res.unwrap()),
         Err(err) => println!("Got error: {}", err),
     } 
+
+    HttpResponse::Ok().body("Pong!")
+}
+
+#[post("/reserves")]
+async fn reserve(req_body: String) -> impl Responder {
+    let addr = ReceiverActor.start();
+    let result = addr.send(ReserveString::new(req_body.clone())).await;
+    match result {
+        Ok(res) => println!("Got result: {}", res.unwrap()),
+        Err(err) => println!("Got error: {}", err),
+    } 
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn version() -> impl Responder {
+    HttpResponse::Ok().body("Version 1.0.0")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(ping)
+            .service(reserve)
+            .route("/version", web::get().to(version))
     })
     .bind("127.0.0.1:8080")?
     .run()
--- tp1-parte-b/src/model/mod.rs
+++ tp1-parte-b/src/model/mod.rs
@@ -1,4 +1,7 @@
 pub mod flight;
 pub mod package;
+pub mod ping_actor;
 pub mod receiver_actor;
+pub mod reserve;
+pub mod reserve_actor;
 pub mod route;
