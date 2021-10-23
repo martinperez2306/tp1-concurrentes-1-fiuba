e5094168cb927f65d4fc5f4150f851d42e7b053c
Martin Nicolas Perez <perezmartincontacto@gmail.com>
2021-10-23 19:05:30 -0300

    Agrego modelo de reserva

---
+++ tp1-parte-b/src/model/reserve.rs
@@ -0,0 +1,34 @@
+use actix::prelude::*;
+
+/// Define message
+#[derive(Message)]
+#[rtype(result = "Result<bool, std::io::Error>")]
+pub struct Reserve {
+    origin: String,
+    destination: String,
+    airline: String,
+    hotel: String,
+}
+
+impl Reserve {
+    pub fn new(origin: String, destination: String, airline: String, hotel: String) -> Reserve {
+        Reserve {
+            origin,
+            destination,
+            airline,
+            hotel
+        }
+    }
+    pub fn get_origin(&self) -> String {
+        self.origin.clone()
+    }
+    pub fn get_destination(&self) -> String {
+        self.destination.clone()
+    }
+    pub fn get_airline(&self) -> String {
+        self.airline.clone()
+    }
+    pub fn get_hotel(&self) -> String {
+        self.hotel.clone()
+    }
+}
